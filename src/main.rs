76673b89a43e8d6183c9b611d059294f0a59bda1
Dan Corneanu <dan.corneanu@youdo.co.nz>
2024-04-28 10:29:45 +1200

    Setup Ad983x device with exclusive SPI bus

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,22 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
+use ad983x::{Ad983x, FrequencyRegister};
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::spi::MODE_0;
+use fugit::RateExtU32;
 use panic_probe as _;
-
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
+    gpio::FunctionSpi,
     pac,
     sio::Sio,
     watchdog::Watchdog,
+    Spi,
 };
 
 #[entry]
@@ -53,23 +50,41 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let sclk = pins.gpio2.into_function::<FunctionSpi>();
+    let mosi = pins.gpio3.into_function::<FunctionSpi>();
+    let cs_pin = pins.gpio16.into_push_pull_output();
+
+    let spi_device = pac.SPI0;
+    let spi_pin_layout = (mosi, sclk);
+
+    let spi = Spi::<_, _, _, 8>::new(spi_device, spi_pin_layout).init(
+        &mut pac.RESETS,
+        125_000_000u32.Hz(),
+        32_000u32.Hz(),
+        MODE_0,
+    );
+
+    let mut dds = Ad983x::new_ad9833(spi, cs_pin);
+
+    dds.reset().unwrap();
+    dds.set_frequency(FrequencyRegister::F0, 4724).unwrap();
+    dds.set_frequency(FrequencyRegister::F1, 4724).unwrap();
+
+    dds.set_output_waveform(ad983x::OutputWaveform::Sinusoidal)
+        .unwrap();
+
+    dds.enable().unwrap();
+
+    // dds.select_frequency(FrequencyRegister::F0).unwrap();
+
+    // Given a 25 MHz clock, this now outputs a sine wave
+    // with a frequency of 440 Hz, which is a standard
+    // A4 tone.
+
+    // Get device back
+    // let _dev = dds.destroy();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
 }
