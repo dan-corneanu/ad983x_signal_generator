fe0f481cdda3c3c869ece844a563c3e51f2ae44c
Dan Corneanu <dan.corneanu@gmail.com>
2024-04-28 10:16:02 +1200

    Update the code to use a shared SPI bus
    Add DelayNs
    
    Remove unnecessary freq channel select
    
    Identify the probe to use
    
    Change CS pin
    
    Use shared SPI bus
    
    Rename project

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use ad983x::{Ad983x, FrequencyRegister};
+use bsp::{entry, hal::Timer};
+use core::cell::RefCell;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use fugit::RateExtU32;
+// use mcp4x::{Channel, Mcp4x};
 use panic_probe as _;
-
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
+    gpio::FunctionSpi,
     pac,
     sio::Sio,
-    watchdog::Watchdog,
+    Spi, Watchdog,
 };
+use embedded_hal::spi::MODE_3;
+use embedded_hal_bus::spi::RefCellDevice;
 
 #[entry]
 fn main() -> ! {
+    let sys_mckl: fugit::Rate<u32, 1, 1> = 125_000_000u32.Hz();
+    let bmc_mckl: fugit::Rate<u32, 1, 1> = 25_000_000u32.Hz();
+    let spi_baud_rate: fugit::Rate<u32, 1, 1> = 32_000u32.Hz();
+
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
+
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    // SIO(single cycle IO)
     let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
@@ -44,6 +49,8 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     let pins = bsp::Pins::new(
@@ -53,23 +60,58 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let sclk = pins.gpio2.into_function::<FunctionSpi>();
+    let mosi = pins.gpio3.into_function::<FunctionSpi>();
+    // let pot_cs_pin = pins.gpio17.into_push_pull_output();
+    let dds_cs_pin = pins.gpio15.into_push_pull_output();
+
+    let spi_device = pac.SPI0;
+    let spi_pin_layout = (mosi, sclk);
+
+    let spi = Spi::<_, _, _, 8>::new(spi_device, spi_pin_layout).init(
+        &mut pac.RESETS,
+        sys_mckl,
+        spi_baud_rate,
+        MODE_3,
+    );
+    let shared_spi_bus = RefCell::new(spi);
+
+    let dds_spi_device = RefCellDevice::new(&shared_spi_bus, dds_cs_pin, timer).unwrap();
+    let mut dds = Ad983x::new_ad9833(dds_spi_device);
+
+    // mcp41x.set_position(Channel::Ch0, 50).unwrap();
+
+    // FREQREG = f * 2^28 / fMCLK
+    info!(
+        "The calculated frequency is: {}",
+        440 * 2u64.pow(28) / (bmc_mckl.to_Hz() as u64)
+    );
+    let f_out: fugit::Rate<u32, 1, 1> = 600u32.Hz();
+    let freqreg0: u32 = (f_out.to_Hz() as u64 * 2u64.pow(28) / (bmc_mckl.to_Hz() as u64))
+        .try_into()
+        .unwrap();
+    let freqreg1: u32 = (f_out.to_Hz() as u64 * 2u64.pow(28) / (bmc_mckl.to_Hz() as u64))
+        .try_into()
+        .unwrap();
+
+    dds.reset().unwrap();
+
+    dds.set_frequency(FrequencyRegister::F0, freqreg0).unwrap();
+    dds.set_frequency(FrequencyRegister::F1, freqreg1).unwrap();
+
+    dds.set_output_waveform(ad983x::OutputWaveform::Triangle)
+        .unwrap();
+
+    dds.enable().unwrap();
+
+    // Given a 25 MHz clock, this now outputs a sine wave
+    // with a frequency of 440 Hz, which is a standard
+    // A4 tone.
+
+    // Get device back
+    // let _dev = dds.destroy();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
 }
